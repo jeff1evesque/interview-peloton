4f7c637fd800b4491f81fc1b34ae3da05d37ba5b
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-02 16:14:18 -0400

    #21: cache_interface.py, define 'Memcached' class

--- logic/cache_interface.py
+++ logic/cache_interface.py
@@ -3,3 +3,25 @@
 ## @cache_interface.py
 #  This file contains the required class to set, get, and delete various
 #      memcached objects.
+import memcache
+from logic.memcache_settings import Memcached_Settings
+
+## Class: Memcached, explicitly inherit 'new-style' class.
+class Memcached(object):
+
+    ## constructor:
+    def __init__(self):
+        self.hostname = Memcached_Settings().get_hostname()
+        self.server   = memcache.Client([self.hostname])
+ 
+    ## set: set value in memcached server.
+    def set(self, key, value, expiry=900):
+        self.server.set(key, value, expiry)
+ 
+    ## get: retrieve value from memcached server.
+    def get(self, key):
+        return self.server.get(key)
+ 
+    ## delete: delete value from memcached server.
+    def delete(self, key):
+        self.server.delete(key)
