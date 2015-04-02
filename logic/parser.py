07c3e1e3fe7e6624f7a6159beca61cd06db0890d
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-02 15:23:27 -0400

    #15: parser.py, add 'get_content' function

--- logic/parser.py
+++ logic/parser.py
@@ -2,3 +2,9 @@
 
 ## @parser.py
 #  This file is responsible for retrieving data from the Peloton API.
+import requests
+
+## get_content: acquires JSON response from Peloton API.
+def get_content(stream):
+    r = requests.get('https://api.pelotoncycle.com/quiz/next/' + stream)
+    return r.json()
