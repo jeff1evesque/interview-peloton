a2ee4ffa379289a1be2260d81830800993119123
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-03 12:12:06 -0400

    #34: app.py, remove duplicate cache logic

--- app.py
+++ app.py
@@ -34,25 +34,6 @@ def quiz():
             content_2 = get_content(stream_1)
 
             # cache stream
-            cached = Memcached()
-            cached_stream_1 = cached.get('cStream1')
-            cached_stream_2 = cached.get('cStream2')
-
-            if cached_stream_1:
-                list_stream_1 = json.loads(cached_stream_1)
-                list_stream_1.append(content_1['current'])
-                cached.set('cStream1', json.dumps(list_stream_1))
-            else:
-                list_stream_1.append(content_1['current'])
-                cached.set('cStream1', json.dumps(list_stream_1))
-
-            if cached_stream_2:
-                list_stream_2 = json.loads(cached_stream_2)
-                list_stream_2.append(content_2['current'])
-                cached.set('cStream2', json.dumps(list_stream_2))
-            else:
-                list_stream_2.append(content_2['current'])
-                cached.set('cStream2', json.dumps(list_stream_2))
 
             # merge streams
             merged_streams = linear_merge(list_stream_1, list_stream_2)
