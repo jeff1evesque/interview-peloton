8e187b0a38722cf6a59549357462357e47cb11bb
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-03 13:21:38 -0400

    #40: memcached_settings.py, define constructor, and method docblocks

--- logic/memcached_settings.py
+++ logic/memcached_settings.py
@@ -7,23 +7,23 @@
 ## Class: Memcached_Settings, explicitly inherit 'new-style' class.
 class Memcached_Settings(object):
 
-    ## constructor:
+    ## constructor: define default memcached host, and port.
     def __init__(self):
         self.host = 'localhost'
         self.port = 5000
 
-    ## get_host
+    ## get_host: return the memcached host.
     def get_host(self):
         return self.host
 
-    ## get_port
+    ## get_port: return the memcached port.
     def get_port(self):
         return self.port
 
-    ## set_host
+    ## set_host: set the memcached host.
     def set_host(self, host):
         self.host = host
 
-    ## set_port
+    ## set_port: set the memcached port.
     def set_port(self, port):
         self.port = port
