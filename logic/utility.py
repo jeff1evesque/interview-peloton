95019c94471d928eeea53df95c2075872adcd511
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-03 12:57:57 -0400

    #37: utility.py, implement 'qsort' in 'linear_merge'

--- logic/utility.py
+++ logic/utility.py
@@ -14,8 +14,13 @@ def qsort(inlist):
         return lesser + [pivot] + greater
 
 ## linear_merge: merge two lists in linear time.
-def linear_merge(list1, list2):
+def linear_merge(list1, list2, sorted=True):
     result = []
+
+    if not sorted:
+        list1 = qsort(list1)
+        list2 = qsort(list2)
+
     while list1 and list2:
         if list1[-1] > list2[-1]:
             result.append(list1.pop())
