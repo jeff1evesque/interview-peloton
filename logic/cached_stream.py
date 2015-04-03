e9375f7493ef1fc0b5dc14854472fb1ba83798d4
Jeff Levesque <jeff1evesque@yahoo.com>
2015-04-03 12:21:39 -0400

    #34: cached_stream.py, add comments

--- logic/cached_stream.py
+++ logic/cached_stream.py
@@ -12,6 +12,7 @@ def cached_stream(content_1, content_2):
     cached_stream_1 = cached.get('cStream1')
     cached_stream_2 = cached.get('cStream2')
 
+    # cache first data stream
     if cached_stream_1:
         list_stream_1 = json.loads(cached_stream_1)
         list_stream_1.append(content_1)
@@ -20,6 +21,7 @@ def cached_stream(content_1, content_2):
         list_stream_1.append(content_1)
         cached.set('cStream1', json.dumps(list_stream_1))
 
+    # cache second data stream
     if cached_stream_2:
         list_stream_2 = json.loads(cached_stream_2)
         list_stream_2.append(content_2)
